8f74a1cdb22249e6b3608ca2279220e5f35daf5b
Kurt Guardia <kurtguardia@gmail.com>
2022-03-25 13:27:49 +0100

    finished

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,3 +1,55 @@
+const cookieToggle = document.getElementById('cookie-toggle');
+const privacyToggle = document.getElementById('privacy-toogle');
+const cookieSection = document.getElementById('cookie');
+const privacySection = document.getElementById('privacy');
+const banner = document.querySelector('.banner');
+const completed = document.getElementById('completed');
+const buttonSubmit = document.querySelector('.btn--cta');
+
+let percentage = 03;
+
+const updatePerc = () => {
+  completed.textContent = `${percentage}%`;
+};
+updatePerc();
+
+banner.addEventListener('click', function (e) {
+  banner.style.display = 'none';
+});
+
+cookieToggle.addEventListener('change', function (e) {
+  const value = e.target.checked;
+  const cookie = cookieSection.style;
+
+  if (value === true) {
+    cookie.display = 'block';
+    percentage += 10;
+  } else {
+    cookie.display = 'none';
+    percentage -= 10;
+  }
+
+  updatePerc();
+});
+
+privacyToggle.addEventListener('change', function (e) {
+  const value = e.target.checked;
+  const privacy = privacySection.style;
+
+  if (value === true) {
+    privacy.display = 'block';
+    percentage += 10;
+  } else {
+    privacy.display = 'none';
+    percentage -= 10;
+  }
+
+  updatePerc();
+});
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////// FOR DROPDOWN MENU  //////////////////////////////////////
+
 const selectWrapper = document.querySelector('.select-wrapper');
 const customOption = document.querySelectorAll('.custom-option');
 const select = document.querySelector('.select');
@@ -28,4 +80,20 @@ window.addEventListener('click', function (e) {
   if (!select.contains(e.target)) {
     select.classList.remove('open');
   }
+});
+
+// Handle Custom theme
+function importData() {
+  let input = document.createElement('input');
+  input.type = 'file';
+  input.onchange = (_) => {
+    // you can use this method to get file and perform respective operations
+    let files = Array.from(input.files);
+    console.log(files);
+  };
+  input.click();
+}
+
+buttonSubmit.addEventListener('click', function () {
+  alert('Successfully submited your data!');
 });
